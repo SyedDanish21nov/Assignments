b65bbff5635c5630693b629c74d137c1901112fe
Syed Danish <69769385+SyedDanish21nov@users.noreply.github.com>
2021-11-03 13:06:04 +0530

    Add files via upload

---
+++ Core Java/Assignment 3/JoinString.java
@@ -0,0 +1,14 @@
+package com.join.string;
+
+public class JoinString {
+
+	public static void main(String[] args) {
+		
+		String str1 ="Hello, ";
+		String str2 ="How are you?";
+		
+		System.out.println(str1.concat(str2));
+
+	}
+
+}
