b50a0c1618ac77e6f8275d34b2421fcdc3c8d041
Syed Danish <69769385+SyedDanish21nov@users.noreply.github.com>
2021-11-08 09:27:51 +0530

    Add files via upload

---
+++ Core Java/Assignment 10/q3/ConvertToArray.java
@@ -0,0 +1,16 @@
+package com.java11.three;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertToArray {
+
+	public static void main(String[] args) {
+	 
+				List<String> str = Arrays.asList("A","quick","brown","fox","jumps","over","the","lazy","dog.");
+				System.out.println(str);
+				str.toArray();
+				str.forEach(l -> System.out.print(l + " "));
+	}
+
+}
