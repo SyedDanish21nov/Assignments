b9825bf2a086af2ec536fe425afbc4e9a37e34b5
Syed Danish <69769385+SyedDanish21nov@users.noreply.github.com>
2021-11-08 09:56:35 +0530

    Add files via upload

---
+++ JUnit 5 & Mockito/q1/MinMaxFinder.java
@@ -0,0 +1,28 @@
+package com.junit.one;
+
+import java.util.Arrays;
+
+public class MinMaxFinder {
+		
+			public int[] findMinMax(int[] a) {
+				int max=0,min=0;
+				int[] arr = new int[] {56,34,7,3,54,3,34,34,53};
+				Arrays.sort(a);
+				
+				arr[0] = a[0];
+				arr[8] = a[a.length-1];
+				
+				return arr;
+				   }
+			public static void main(String[] args) {
+	
+				MinMaxFinder obj = new MinMaxFinder();
+		          
+		         int[] a =  obj.findMinMax(new int[] {56,34,7,3,54,3,34,34,53});
+		         
+		         System.out.println(a[0]);
+		         System.out.println(a[8]);
+		
+	}
+
+}
